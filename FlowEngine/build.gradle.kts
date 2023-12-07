6407bf112bf5500cb5c749d1e20b5beed9f4465f
JUST DOINS <34908257+justdoins@users.noreply.github.com>
2023-12-07 21:40:24 +0800

    initial logback slf4j log

--- FlowEngine/build.gradle.kts
+++ FlowEngine/build.gradle.kts
@@ -12,12 +12,27 @@ repositories {
 object VER {
     const val LOGGER = "5.1.0"
     const val SLF4J = "2.0.9"
+    const val LOGBACK = "1.4.8"
+
+
+    /**
+     * Deprecated
+     */
     const val LOG4J = "2.20.0"
 }
 
 dependencies {
+    /**
+     * slf4j-impl only need one
+     */
     implementation("org.slf4j:slf4j-api:${VER.SLF4J}")
-    implementation("org.slf4j:slf4j-simple:${VER.SLF4J}")
+//    implementation("org.slf4j:slf4j-simple:${VER.SLF4J}")
+    implementation("ch.qos.logback:logback-classic:${VER.LOGBACK}")
+//    implementation("org.slf4j:slf4j-reload4j:${VER.SLF4J}")
+//    implementation("org.slf4j:slf4j-jcl:${VER.SLF4J}")
+    /**
+     * Deprecated
+     */
 //    implementation("org.apache.logging.log4j:log4j-slf4j-impl:${VER.LOG4J}")
     implementation("io.github.oshai:kotlin-logging-jvm:${VER.LOGGER}")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
--- FlowEngine/src/main/kotlin/org/dustbins/main/Main.kt
+++ FlowEngine/src/main/kotlin/org/dustbins/main/Main.kt
@@ -1,28 +1,39 @@
 package org.dustbins.main
 
 import io.github.oshai.kotlinlogging.KotlinLogging
+import kotlin.concurrent.thread
 
 //import mu.KotlinLogging
 private val logger = KotlinLogging.logger {}
 private const val name = "FlowEngine"
 
 /**
- * start code from here
+ * start code from here,from head to toe.
+ * normally we don't do this in class.
  */
 class Main {
 
 
+    /**
+     *
+     *
+     * @param
+     *
+     * @return
+     */
     fun loadSpace() {
-        
+
     }
 
-
 }
-
+fun doNothing(){
+    thread {  }
+}
 /**
  * if there is a main entrance
  */
 fun main() {
     //logger
-    logger.info { "welcome to $name" }
+    logger.info { "welcome using $name" }
+
 }
